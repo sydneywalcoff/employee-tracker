99951083b5a91de8a8be937c2ba129a69639baa3
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-20 14:29:48 -0700

    moved managers to top of seed to fix error

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,6 +10,6 @@ INSERT INTO roles (title, salary, department_id) VALUES
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
     ('Sydney', 'Walcoff', 3, NULL),
-    ('Liba', 'Dogg', 2, 3),    
     ('Salma', 'Abdelfattah', 4, NULL),
-    ('Lily', 'Pooch', 1, 4); 
+    ('Liba', 'Dogg', 2, 1),   
+    ('Lily', 'Pooch', 1, 2); 
