b0ecdfdaf19368b04d926e8b37b1bac46052efd4
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-19 17:22:22 -0700

    added to seeds and fixed problems with schema; problems with seeds currently

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,26 @@
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS employees;
 
-CREATE TABLE department(
+CREATE TABLE departments (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles(
+CREATE TABLE roles (
     id INTEGER PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employees(
-    id INTEGER AUTO-INCREMENT PRIMARY KEY,
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    manager_id INTEGER
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO departments VALUES 
+    (1, 'marketing'),
+    (2, 'engineering');
+    
+INSERT INTO roles VALUES
+    (1, 'copywriter', 60000.00, 1),
+    (2, 'jr engineer', 75000.00, 2),
+    (3, 'sr engineer', 100000.00, 2),
+    (4, 'marketing director', 90000.00, 1);
+
+INSERT INTO employees VALUES
+    (1, 'Sydney', 'Walcoff', 3, NULL),
+    (2, 'Liba', 'Dogg', 2, 3),    
+    (3, 'Salma', 'Abdelfattah', 4, NULL),
+    (4, 'Lily', 'Pooch', 1, 4); 
